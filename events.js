f9b244e81f70d1cae120e9bb7f744ca01c516aea
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2020-03-28 20:14:11 -0300

    First commit

---
+++ events.js
@@ -0,0 +1,9 @@
+"use strict"
+
+function nomeDaFuncao(param1, param2) {
+    return param1 + param2;
+}
+
+function outraFuncao(param1, param2) {
+    console.log(nomeDaFuncao(param1, param2) * param1 / param2);
+}
