1f106f7ecdf32c980ade65043516d6b89bf639fb
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2020-04-15 21:17:05 -0300

    Ajuste html pagina index

--- events.js
+++ events.js
@@ -1,9 +1,1 @@
 "use strict"
-
-function nomeDaFuncao(param1, param2) {
-    return param1 + param2;
-}
-
-function outraFuncao(param1, param2) {
-    console.log(nomeDaFuncao(param1, param2) * param1 / param2);
-}
