5958ef6c7922da11ab5530b0039b525106cd9140
Anderson <anderson@viajandodebarraca.com.br>
2020-04-15 21:46:17 -0300

    estilizando menu

--- events.js
+++ events.js
@@ -1,9 +1,3 @@
-"use strict"
+'use strict';
 
-function nomeDaFuncao(param1, param2) {
-    return param1 + param2;
-}
-
-function outraFuncao(param1, param2) {
-    console.log(nomeDaFuncao(param1, param2) * param1 / param2);
-}
+import("./assets/js/slide.js");
